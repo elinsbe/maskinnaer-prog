e8a1ecb17be4ea0d1e9d0e581b40bdc74a740677
Elin Skaanlund Berntsen <elinsberntsen@gmail.com>
2024-10-08 14:46:52 +0200

    nå blir det hvit

--- oving1/palin_finder.s
+++ oving1/palin_finder.s
@@ -1,36 +1,23 @@
 .global _start
 
 _start:
-	//reset all registers to 0
-	MOV R0, #0
-	MOV R1, #0
-	MOV R2, #0
-	MOV r3, #0
-	mov r4, #0
-	mov r5, #0
-	mov r6, #0
-	mov r7, #0
-	mov r11, #0
-	mov r12, #0
 	
-	ldr R7, LED_BASE
-
-	// bytes we add/subtract to move up and down address
-	mov r3, #1
-	
-	//r0 will have the base address. DO NOT CHANGE UNTIL ITERATION
+	//r0 will have the base address. 
+	// Points to leftmost byte
 	ldr r0, =input
 	
-	// We will iterate over r1 to find end address.
+	// Will eventually point to the address of the last char
 	ldr r1, =input
 	
-	// R2 will be used to iterate through
-	// the string and check when it ends
+	//Will hold value of string
 	ldrb r2, [r1]
 	
 	//r4 will be amount of bytes moved/length of string
 	mov r4, #0
 	
+	
+	//adresses and/or words
+	ldr R7, LED_BASE
 	ldr r8, =detected
 	ldr r9, =not_detected
 	ldr r10, JTAG
@@ -46,8 +33,9 @@ _start:
 
 //Here we will get to the end of the string
 //need to find end charaters
+// Will find end by comparing to the null byte
 get_end_of_string:
-	add R1, r1, r3
+	add R1, r1, #1
 	add r4, r4, #1
 	
 	ldrb r2, [r1]
@@ -56,20 +44,19 @@ get_end_of_string:
 	
 	// R1 will now hold the address of the null byte
 	//must subtract 1 byte to find last character
-
 	sub r1, r1, #1
-	//sub r4, r4, #1
-	// Now we are going to iterate
-	// over address and string
 	
-
+//If the char is a space, we will iterate jump until we find non-space char
+//we know that r4 has the length of string. will move until r4 is 1 (string is odd) or 0 (string is even)
 check_leftside:
 	ldrb r5, [r0]
 	sub r4, r4, #1
 	add r0, r0, #1
+	
 	cmp r5, #0x00000020
 	beq check_leftside
-	
+
+//Same here, but moving right to left
 check_rightside:
 	ldrb r6, [r1]
 	sub r4, r4, #1
@@ -77,71 +64,62 @@ check_rightside:
 	
 	cmp r6, #0x00000020
 	beq check_rightside
-	
 
-	
+
 check_input:
-	
+	// If the char is ? then it will have the same value as the char we are comparing it to
 	cmp r5, #0x0000003f
 	moveq r5, r6
 	
 	cmp r6, #0x0000003f
 	moveq r6, r5
 	
+	//If the chars are lowercase, which is all chars equal or greater than 97, then we subtract to make lower
 	cmp r5, #97
 	subge r5, r5, #32
 	
 	cmp r6, #97
 	subge r6, r6, #32
 	
-
+	//If the two chars now are NOT equal, the word will never be a palindrome
 	cmp r6, r5
 	bne not_palindrome
-	
 	
 	// if the counter is larger than 1 we still need to iterate!
 	CMP r4, #1
 	bge check_leftside
-	
-	
-	//if r4 is equal, then it is a odd-length string
-	//cmp r4, #1
-	//bne check_input
-	
 	b palindrome
-	
-	
-	b _exit
 	
 palindrome:
 	ldrb r2, [r8]
 	str r2, [r10]
 	
-	cmp r2, #0
-	beq _exit
-	
 	add r8, r8, #1
 	
-	b palindrome
+	cmp r2, #0
+	bne palindrome
 	
+	//add newline
+	mov r2, #10
+	str r2, [r10]
+	
+	//sets the leds
 	add R11, r7, #992
 	str r11, [r7]
-	
-	
-	
 	
 	b _exit
 
 not_palindrome:	
 	ldrb r2, [r9]
 	str r2, [r10]
-	
-	cmp r2, #0
-	beq _exit
-	
 	add r9, r9, #1
 	
-	b not_palindrome
+	cmp r2, #0
+	bne not_palindrome
+	
+	mov r2, #10
+	str r2, [r10]
+	
 	
 	add R11, r7, #31
 	str r11, [r7]
@@ -163,10 +141,10 @@ _exit:
 
 
 .align
-	input: .asciz "X     bbb"
+	input: .asciz "Grav ned den varg"
 	
-	detected: .asciz "Palindrome detected"
-	not_detected: .asciz "Not a palindrome"
+	detected: .asciz "Palindrome detected\n"
+	not_detected: .asciz "Not a palindrome\n"
 	
 	
 	
